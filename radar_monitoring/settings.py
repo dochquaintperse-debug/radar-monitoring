7c7fdce764fb1ba9452d0f922035b08b72e5b7a1
Wax <dochquaintperse@gmail.com>
2025-10-04 21:53:20 +0800

    🔧 Fix Django templates: add missing messages context processor

--- radar_monitoring/settings.py
+++ radar_monitoring/settings.py
@@ -59,18 +59,19 @@ ROOT_URLCONF = 'radar_monitoring.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.messages',
+                'django.contrib.messages.context_processors.messages', 
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'radar_monitoring.wsgi.application'
 ASGI_APPLICATION = "radar_monitoring.asgi.application"
